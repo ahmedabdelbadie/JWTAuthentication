2f6afa90dd51b6bafca19fb8ccc1520e71ab6983
AhmedBadea <ahmed.elsayed@dafater.sa>
2022-07-04 16:22:31 +0200

    Add project files.

---
+++ JWTAuthentication/APP_DATA/IEmployees.cs
@@ -0,0 +1,14 @@
+﻿using JWTAuthentication.Models;
+
+namespace JWTAuthentication.APP_DATA
+{
+    public interface IEmployees
+    {
+        public List<Employee> GetEmployeeDetails();
+        public Employee GetEmployeeDetails(int id);
+        public void AddEmployee(Employee employee);
+        public void UpdateEmployee(Employee employee);
+        public Employee DeleteEmployee(int id);
+        public bool CheckEmployee(int id);
+    }
+}
---
+++ JWTAuthentication/Program.cs
@@ -0,0 +1,50 @@
+using JWTAuthentication.APP_DATA;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DatabaseContext>
+    (options => options.UseSqlServer(builder.Configuration.GetConnectionString("dbConnection")));
+builder.Services.AddTransient<IEmployees, EmployeeRepository>();
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters()
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
+var app = builder.Build();
+
+
+app.UseAuthentication();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
